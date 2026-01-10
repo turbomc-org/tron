552489213e49f6ce4b0fc0153639bdf2260734f8
harihar-nautiyal <nautiyalhn@gmail.com>
2026-01-10 22:37:12 +0530

    feat: Backup

--- tron/src/modules/leaderboard.rs
+++ tron/src/modules/leaderboard.rs
@@ -1,23 +1,22 @@
 use crate::models::leaderboards::Leaderboard;
 use crate::models::leaderboards::ScoreKey;
 use anyhow::Result;
-use std::borrow::Borrow;
 use std::cmp::Reverse;
 
 impl Leaderboard {
     pub async fn update_score(&self, player_id: u64, new_score: u64) {
+        let mut order = self.order.write().await;
         if let Some(old_key) = self.index.remove(&player_id) {
-            self.order.write().await.remove(&old_key.1);
+            order.remove(&old_key.1);
         }
         let key = ScoreKey(Reverse(new_score), player_id);
         self.index.insert(player_id, key.clone());
-        self.order.write().await.insert(key, new_score);
+        order.insert(key, new_score);
     }
 
-    pub async fn get_rank(&self, player_id: impl Borrow<u64>) -> Option<usize> {
-        let player_id: &u64 = player_id.borrow();
-        let key = self.index.get(player_id)?.clone();
+    pub async fn get_rank(&self, player_id: &u64) -> Option<usize> {
         let order = self.order.read().await;
+        let key = self.index.get(player_id)?.clone();
         Some(order.rank(&key) + 1)
     }
 
@@ -32,13 +31,12 @@ impl Leaderboard {
     }
 
     pub async fn get_score(&self, player_id: u64) -> Result<Option<u64>> {
-        let key = self.index.get(&player_id);
-
-        if key.is_none() {
-            return Ok(None);
-        }
+        let key_ref = match self.index.get(&player_id) {
+            Some(k) => k,
+            None => return Ok(None),
+        };
 
         let order = self.order.read().await;
-        Ok(order.get(&key.unwrap()).cloned())
+        Ok(order.get(&key_ref).cloned())
     }
 }
